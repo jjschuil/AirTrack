770e468c7cef576957d01617dd72bbd0c1ea7c1c
Jackson Schuiling <jacksonjschuiling@gmail.com>
2023-03-16 09:58:13 -0400

    first files

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Jackson Schuiling on 3/16/23.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Jackson Schuiling on 3/16/23.
+//
+
+#ifndef AIRTRACK_MAIN_H
+#define AIRTRACK_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //AIRTRACK_MAIN_H
